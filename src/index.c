5c1a7838d31d603f52c1df2f68ab941ddb30fb28
Eqilia <eqilia@waifu.club>
2024-04-19 20:17:07 +0200

    chore: Remove the extracted registering function from src/index.c

--- src/index.c
+++ src/index.c
@@ -2,7 +2,6 @@
 #include <string.h> // strcmp(3)
 #include <unistd.h> // getpid(3)
 #include <sys/types.h> // pid_t
-#include <signal.h> // signal(3)
 #include <stdlib.h> // exit(3)
 
 #include <concord/discord.h>
@@ -10,29 +9,14 @@
 #include "../headers/config.h"
 
 void on_ready(struct discord *client, const struct discord_ready *event) {
-	struct discord_create_guild_application_command params = {
-		.name = "ping",
-		.description = "Ping command!"
-	};
-	int pid = getpid();
+	printf("%s\n", "Ready event emitted!");
 
-	discord_create_guild_application_command(client, event->application->id, GUILD_ID, &params, NULL);
-
-	printf("Registered ping!\n");
-
-	kill(pid, SIGUSR1);
+	printf("%s\n", event->application->name);
 }
 
-void sigusro_handler(int sig)
+int main()
 {
-	signal(sig, SIG_IGN);
-	exit(0);
-}
-
-int main(void) {
 	struct discord *client = discord_init(BOT_TOKEN);
-
-	signal(SIGUSR1, sigusro_handler);
 
 	discord_set_on_ready(client, &on_ready);
 	discord_run(client);
