c0af92c44a1ba0c7ab653530ebed130f426c0a14
Eqilia <trulyeqilia@gmail.com>
2024-05-18 18:48:59 +0200

    chore: Remove useless dynamic strings
    TL;DR remove possible attack vectors
    
    Signed-off-by: Eqilia <trulyeqilia@gmail.com>

--- src/commands/ping.c
+++ src/commands/ping.c
@@ -1,5 +1,4 @@
 #include <stdio.h> // printf(3)
-#include <string.h> // strcat(3), strerror
 #include <sys/utsname.h>
 
 #include "../../headers/constants/oslimits.h"
@@ -10,43 +9,13 @@
 
 void on_interaction_command_ping(struct discord *client, const struct discord_interaction *event)
 {
-	struct utsname unameData;
-	int unameCode;
-	char content[2000];
-
-	memset(content, '\0', 2000);
-
-	unameCode = uname(&unameData);
-
-	if (unameCode != 0 && errno != 0) {
-		printf("%s\n", strerror(errno));
-
-		struct discord_interaction_response params = {
-			.type = DISCORD_INTERACTION_CHANNEL_MESSAGE_WITH_SOURCE,
-			.data = &(struct discord_interaction_callback_data){
-				.content = "Failed!"
-			}
-		};
-
-		printf("%s\n", unameData.sysname);
-
-		discord_create_interaction_response(client, event->id, event->token, &params, NULL);
-	}
-
-	strcat(content, "Hello from ");
-	strcat(content, unameData.sysname);
-	strcat(content, " ");
-	strcat(content, unameData.release);
-	strcat(content, " (");
-	strcat(content, unameData.machine);
-	strcat(content, ")!");
-
 	struct discord_interaction_response params = {
 		.type = DISCORD_INTERACTION_CHANNEL_MESSAGE_WITH_SOURCE,
 		.data = &(struct discord_interaction_callback_data){
-			.content = content
+			.content = "hi"
 		}
 	};
+
 	discord_create_interaction_response(client, event->id, event->token, &params, NULL);
 }
 
